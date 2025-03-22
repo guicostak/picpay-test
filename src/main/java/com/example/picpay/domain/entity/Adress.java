97222c84ff6571b109f6c414308bf90ad32a0761
Guilherme Augustto <guilhermecosta.barros@gmail.com>
2025-03-22 10:14:11 -0300

    feat: incrementing adress entity

--- src/main/java/com/example/picpay/domain/entity/Adress.java
+++ src/main/java/com/example/picpay/domain/entity/Adress.java
@@ -1,14 +1,23 @@
 package com.example.picpay.domain.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Entity
+@Builder
 public class Adress {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    private String city;
+    private String state;
     private String street;
+    private String postalCode;
+    private Long number;
 }
